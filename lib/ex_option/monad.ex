dd9889736768be7584faee09bee7b5a6ee5ad2bd
Marijn van Wezel <96489967+marijnvanwezel@users.noreply.github.com>
2025-06-26 13:33:10 +0200

    Change operator to `~>>/2`
    Signed-off-by: Marijn van Wezel <96489967+marijnvanwezel@users.noreply.github.com>

--- lib/ex_option/monad.ex
+++ lib/ex_option/monad.ex
@@ -17,36 +17,36 @@ defmodule ExOption.Monad do
 
   ## Examples
 
-      iex> {:ok, "Hello!"} ~> String.reverse()
+      iex> {:ok, "Hello!"} ~>> String.reverse()
       "!olleH"
 
-      iex> {:some, "Hello!"} ~> String.reverse()
+      iex> {:some, "Hello!"} ~>> String.reverse()
       "!olleH"
 
-      iex> {:error, "Hello!"} ~> String.reverse()
+      iex> {:error, "Hello!"} ~>> String.reverse()
       {:error, "Hello!"}
 
-  The `~>/2` operator is mostly useful when there is a desire to execute a series
+  The `~>>/2` operator is mostly useful when there is a desire to execute a series
   of operations resembling a pipeline, where some or all of the operations may fail:
 
-      iex> {:ok, "Hello!"} ~> (fn _ -> error("Failed!") end).() ~> (fn value -> ok(value) end).()
+      iex> {:ok, "Hello!"} ~>> (fn _ -> error("Failed!") end).() ~>> (fn value -> ok(value) end).()
       {:error, "Failed!"}
 
-      iex> {:ok, "Hello"} ~> (fn value -> ok(value <> ", world!") end).()
+      iex> {:ok, "Hello"} ~>> (fn value -> ok(value <> ", world!") end).()
       {:ok, "Hello, world!"}
 
-      iex> {:ok, "A"} ~> (fn value -> ok(value <> "B") end).() ~> (fn value -> ok(value <> "C") end).()
+      iex> {:ok, "A"} ~>> (fn value -> ok(value <> "B") end).() ~>> (fn value -> ok(value <> "C") end).()
       {:ok, "ABC"}
 
   If the function on the left-hand side of the operator does not return a monadic
   value (e.g. not an `:ok`/`:error`/`:some` tuple or `:none`), then the resulting
   value will also not be a monadic value.
 
-      iex> ok("Hello!") ~> String.reverse()
+      iex> ok("Hello!") ~>> String.reverse()
       "!olleH"
 
   """
-  defmacro left ~> right do
+  defmacro left ~>> right do
     quote do
       bind(unquote(left), fn val ->
         unquote(Macro.pipe(quote(do: val), right, 0))
